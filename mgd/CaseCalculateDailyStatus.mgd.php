f90f2659cc6a5ca1d5f7f1cd616d05780b7d3065
Matthew Wire (MJW Consulting) <mjw@mjwconsult.co.uk>
2019-12-08 22:21:23 +0000

    Add scheduled job for calculating case totals

---
+++ mgd/CaseCalculateDailyStatus.mgd.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+  0 =>
+    [
+      'name' => 'Calculate Daily Case Status Totals',
+      'entity' => 'CaseStatistics',
+      'params' =>
+        [
+          'version' => 3,
+          'name' => 'CalculateDailystatus',
+          'description' => 'Calculate the daily totals for case status and output to civicrm_statistics_casestatus_daily table',
+          'run_frequency' => 'Daily',
+          'api_entity' => 'CaseStatistics',
+          'api_action' => 'calculate_dailystatus',
+          'parameters' => '',
+        ],
+    ],
+];
