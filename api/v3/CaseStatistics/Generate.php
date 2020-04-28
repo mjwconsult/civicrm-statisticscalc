55d0bbf9c73fac5a0edb86eb6402acc6163f9004
Matthew Wire <mjw@mjwconsult.co.uk>
2020-04-28 11:55:59 +0100

    Add copyright headers

--- api/v3/CaseStatistics/Generate.php
+++ api/v3/CaseStatistics/Generate.php
@@ -1,6 +1,13 @@
 <?php
-
-use CRM_Statisticscalc_ExtensionUtil as E;
+/*
+ +--------------------------------------------------------------------+
+ | Copyright CiviCRM LLC. All rights reserved.                        |
+ |                                                                    |
+ | This work is published under the GNU AGPLv3 license with some      |
+ | permitted exceptions and without any warranty. For full license    |
+ | and copyright information, see https://civicrm.org/licensing       |
+ +--------------------------------------------------------------------+
+ */
 
 /**
  * CaseStatistics.Generate API specification (optional)
