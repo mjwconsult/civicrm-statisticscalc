ef5091a76356b493bc3a09bf0aac21d7d7b554d9
Matthew Wire <mjw@mjwconsult.co.uk>
2024-01-29 21:46:24 +0000

    Upgrade civix

--- statisticscalc.php
+++ statisticscalc.php
@@ -37,59 +37,12 @@ function statisticscalc_civicrm_install() {
 }
 
 /**
- * Implements hook_civicrm_postInstall().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_postInstall
- */
-function statisticscalc_civicrm_postInstall() {
-  _statisticscalc_civix_civicrm_postInstall();
-}
-
-/**
- * Implements hook_civicrm_uninstall().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_uninstall
- */
-function statisticscalc_civicrm_uninstall() {
-  _statisticscalc_civix_civicrm_uninstall();
-}
-
-/**
  * Implements hook_civicrm_enable().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_enable
  */
 function statisticscalc_civicrm_enable() {
   _statisticscalc_civix_civicrm_enable();
-}
-
-/**
- * Implements hook_civicrm_disable().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_disable
- */
-function statisticscalc_civicrm_disable() {
-  _statisticscalc_civix_civicrm_disable();
-}
-
-/**
- * Implements hook_civicrm_upgrade().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_upgrade
- */
-function statisticscalc_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _statisticscalc_civix_civicrm_upgrade($op, $queue);
-}
-
-/**
- * Implements hook_civicrm_entityTypes().
- *
- * Declare entity types provided by this module.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_entityTypes
- */
-function statisticscalc_civicrm_entityTypes(&$entityTypes) {
-  _statisticscalc_civix_civicrm_entityTypes($entityTypes);
 }
 
 /**
